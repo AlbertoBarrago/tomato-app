4ac9b3e0b93ec6188c8b6b402b48088942605959
Alberto Barrago <albertobarrago@gmail.com>
2024-10-20 23:05:11 +0200

    fixes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import {TabBarIcon} from '@/components/navigation/TabBarIcon';
 import {Colors} from '@/constants/Colors';
 import {useColorScheme} from '@/hooks/useColorScheme';
-import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
